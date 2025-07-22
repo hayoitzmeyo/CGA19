57a7cf7faf7abef5c042fd8245ceefa0bf0e5f4c
hayoitzmeyo <chejohnson023@gmail.com>
2025-07-21 17:41:06 -0700

    debugging 131

--- Renderapi/__init__.py
+++ Renderapi/__init__.py
@@ -3,10 +3,10 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
-    # Register blueprints
+    CORS(app, origins=["https://cga19.netlify.app"])
     from .backend import bp as afq_bp
     from .firealg import bp as firealg_bp
     app.register_blueprint(afq_bp)
     app.register_blueprint(firealg_bp)
     return app
+
