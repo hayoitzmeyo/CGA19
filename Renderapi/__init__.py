0004918b5ccc7eb3b79300332c38c167bda430f4
hayoitzmeyo <chejohnson023@gmail.com>
2025-10-21 14:37:53 -0700

    cors fix

--- Renderapi/__init__.py
+++ Renderapi/__init__.py
@@ -3,10 +3,18 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["https://georisk.netlify.app"])
+    
+    CORS(
+        app,
+        resources={r"/*": {"origins": ["https://georisk.netlify.app"]}},
+        supports_credentials=True,
+        allow_headers=["Content-Type", "Authorization"],
+        methods=["GET", "POST", "OPTIONS"]
+    )
+    
     from .backend import bp as afq_bp
     from .firealg import bp as firealg_bp
     app.register_blueprint(afq_bp)
     app.register_blueprint(firealg_bp)
+    
     return app
-
